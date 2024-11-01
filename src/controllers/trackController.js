6868d98567a8e44ca91546cef86d4af0c042edbd
Eliezer Nsengi <nsengitech@gmail.com>
2024-11-01 15:42:54 +0200

    add: track controller init

---
+++ src/controllers/trackController.js
@@ -0,0 +1,30 @@
+const Track = require("../models/Track");
+
+exports.createTrack = async (req, res) => {
+  try {
+    const { title, featuring } = req.body;
+    const coverImage = req.files["cover"][0].path;
+    const audioFile = req.files["audio"][0].path;
+
+    const track = new Track({
+      title,
+      featuring,
+      coverImage,
+      audioFile,
+    });
+
+    await track.save();
+    res.status(201).json(track);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+exports.getTracks = async (req, res) => {
+  try {
+    const tracks = await Track.find().sort({ releaseDate: -1 });
+    res.json(tracks);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
