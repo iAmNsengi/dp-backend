939fa75e42ff3bb5b462c86aebb5e5f463c0fe22
Eliezer Nsengi <nsengitech@gmail.com>
2024-11-01 15:39:51 +0200

    add: Track model

---
+++ src/models/Track.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const trackSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    featuring: {
+      type: String,
+    },
+    coverImage: {
+      type: String,
+      required: true,
+    },
+    audioFile: {
+      type: String,
+      required: true,
+    },
+    releaseDate: {
+      type: Date,
+      default: Date.now(),
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Track", trackSchema);
