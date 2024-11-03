cb71557a0297224630ea2b9f0f55ca7a8fc604bb
KIBONGO Simon Peter <kibongos40@gmail.com>
2024-11-03 15:18:26 +0200

    Mongidb connection cleanup
    Start App after mongodb Successful connection

--- src/app.js
+++ src/app.js
@@ -13,11 +13,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use("/src/uploads", express.static(path.join(__dirname, 'uploads')));
 
-// Database connection
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+// App Config
 
 // Routes
 app.use("/api/tracks", require("./routes/trackRoutes"));
@@ -26,4 +22,18 @@ app.use("/api/events", require("./routes/eventRoutes"));
 app.use("/api/auth", require("./routes/authRoutes"));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, 'localhost', () => console.log(`Server running on port ${PORT}`));
+
+// Database connection
+mongoose
+  .connect(process.env.MONGODB_URI,
+  {
+    serverSelectionTimeoutMS: 30000
+  }
+    )
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, 'localhost', () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+  
